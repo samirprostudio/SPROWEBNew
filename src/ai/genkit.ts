dbe6275e30f6762b7640b475d93be71bb5c75734
SAMIR <samirpro.info@gmail.com>
2025-09-22 02:43:22 +0600

    New Again

---
+++ src/ai/genkit.ts
@@ -0,0 +1,11 @@
+import {genkit} from 'genkit';
+import {googleAI} from '@genkit-ai/googleai';
+import { config } from 'dotenv';
+
+config();
+
+export const ai = genkit({
+  plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  })],
+});
---
+++ src/app/api/genkit/[[...path]]/route.ts
@@ -0,0 +1,7 @@
+'use server';
+import { createNextApiHandler } from '@genkit-ai/next';
+import { ai } from '@/ai/genkit';
+
+export const { GET, POST } = createNextApiHandler({
+  ai,
+});
